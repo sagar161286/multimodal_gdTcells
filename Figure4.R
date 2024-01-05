5e5405f94b52f3bbe49223c2e507748b496759a5
Sagar <73115079+sagar161286@users.noreply.github.com>
2024-01-05 13:24:39 +0100

    Update Figure4.R

--- Figure4.R
+++ Figure4.R
@@ -1,13 +1,137 @@
-# download gd_holygrail_filtered_final.rds from here: https://drive.google.com/file/d/16SeXuyOkRm3y33TqxwIcWewmBY_Nbpfw/view?usp=share_link
+library(tidyverse)
+library(scran)
+library(patchwork)
+library(viridis)
+library(ggforce)
+library(gghalves)
+library(ggridges)
+library(scDblFinder)
+library(SingleR)
+library(wesanderson)
+library(pheatmap)
+library(Signac)
+library(Seurat)
+library(JASPAR2020)
+library(TFBSTools)
+library(BSgenome.Mmusculus.UCSC.mm10)
+library(patchwork)
 
-multimodal_gd_gex <- readRDS("gd_holygrail_filtered_final.rds")
+# download combined_commonpeaks_hm_integrated_only_atac.rds from here: https://drive.google.com/file/d/1LewftPzazsR1Ozy5rp-he2syG18Gr5L1/view?usp=share_link
 
-#figure 4a
-Idents(multimodal_gd_gex) <- "celltypes"
-factor(Idents(multimodal_gd_gex), levels= c("others" ,"Areg+","Rorc+","Proliferating","Klrg1+ effector","Gzmb+ IELs","Cd160+","Sell+Ly6c+","Sell+Ly6c-"))
-Idents(multimodal_gd_gex) <- factor(Idents(multimodal_gd_gex), levels= c("others" ,"Areg+","Rorc+","Proliferating","Klrg1+ effector","Gzmb+ IELs","Cd160+","Sell+Ly6c+","Sell+Ly6c-"))
+gd_atac <- readRDS("combined_commonpeaks_hm_integrated_only_atac.rds")
 
-DotPlot(multimodal_gd_gex, features = c("Sell","Ly6c2","Cd160","Gzmb","Klrg1","Mki67","Rorc","Areg"),cols = c("RdYlBu")) + geom_point(aes(size=pct.exp), shape = 21, colour="black", stroke=0.5) + guides(size=guide_legend(override.aes=list(shape=21, colour="black", fill="white"))) + RotatedAxis() +theme(
+
+# FIGURE 4A
+
+custom_colors <- list()
+colors_dutch <- c(
+  '#FFC312','#C4E538','#12CBC4','#FDA7DF','#ED4C67',
+  '#A3CB38','#1289A7','#D980FA','#B53471',
+  '#EE5A24','#009432','#0652DD','#9980FA','#833471',
+  '#EA2027','#006266'
+)
+
+set.seed(123)
+clust.col = sample(rainbow(5))
+custom_colors$discrete <- c(colors_dutch, clust.col)
+
+UMAP_centers_cluster <- tibble(
+  UMAP_1 = as.data.frame(gd_atac@reductions$umap@cell.embeddings)$UMAP_1,
+  UMAP_2 = as.data.frame(gd_atac@reductions$umap@cell.embeddings)$UMAP_2,
+  cluster = gd_atac@meta.data$seurat_clusters
+) %>%
+  group_by(cluster) %>%
+  summarize(x = median(UMAP_1), y = median(UMAP_2))
+
+bind_cols(gd_atac@meta.data, as.data.frame(gd_atac@reductions$umap@cell.embeddings)) %>%
+  ggplot(aes(UMAP_1, UMAP_2, color = seurat_clusters)) +
+  geom_point(size = 0.2) +
+  geom_label(
+    data = UMAP_centers_cluster,
+    mapping = aes(x, y, label = cluster),
+    size = 7,
+    fill = 'white',
+    color = 'black',
+    fontface = 'bold',
+    alpha = 0.8,
+    label.size = 0,
+    show.legend = FALSE
+  ) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),  panel.background = element_blank())+
+  scale_color_manual(
+    name = 'Cluster', values = custom_colors$discrete,
+    guide = guide_legend(override.aes = list(size = 2))
+  )
+
+
+# FIGURE 4B
+
+DimPlot(gd_atac, group.by = 'holygrail.celltype', pt.size = 0.1, cols = c('#FFC312','#C4E538','#12CBC4','#FDA7DF','grey','#ED4C67','#1289A7','#D980FA','#B53471'))
+
+# FIGURE 4C
+
+da_peaks_all <- readRDS("/data/gruen/sagar2/R/gd_multiome_scRNAseq_scATACseq/da_peaks_all_clusters.rds")
+avg.exp <- AverageExpression(gd_atac, group.by = "ident", slot="data", return.seurat = F, features = unique(da_peaks_all$gene))
+avg.exp <- as.data.frame(avg.exp$ATAC)
+avg.exp[avg.exp > 5] <- 5
+
+genes <- ClosestFeature(gd_atac, regions = rownames(avg.exp))
+genes$heatmap_id <- paste(genes$query_region, genes$gene_name, sep = "_")
+rownames(genes) <- genes$query_region
+heatmap_dataframe <- merge(genes, avg.exp, by = "row.names")
+rownames(heatmap_dataframe) <- heatmap_dataframe$heatmap_id
+heatmap_dataframe <- heatmap_dataframe[,c(11:27)]
+heatmap_dataframe2 <- heatmap_dataframe[, c(1,6,11,3,4,8,5,2,10,13,14,9,12,16,17,15,7)]
+pheatmap(heatmap_dataframe2, color = wes_palette("Zissou1", 10, type = "continuous"),show_rownames = F, fontsize_row = 1, cluster_rows = T, cluster_cols = T)
+
+# FIGURE 4D
+
+set.seed(1234)
+
+DefaultAssay(gd_atac) <- "ATAC"
+
+main.chroms <- standardChromosomes(BSgenome.Mmusculus.UCSC.mm10)
+keep.peaks <- which(as.character(seqnames(granges(gd_atac))) %in% main.chroms)
+gd_atac[["ATAC"]] <- subset(gd_atac[["ATAC"]], features = rownames(gd_atac[["ATAC"]])[keep.peaks])
+
+# Get a list of motif position frequency matrices from the JASPAR database
+pfm <- getMatrixSet(
+  x = JASPAR2020,
+  opts = list(collection = "CORE", tax_group = 'vertebrates', all_versions = FALSE)
+)
+
+# add motif information
+gd_atac <- AddMotifs(
+  object = gd_atac,
+  genome = BSgenome.Mmusculus.UCSC.mm10,
+  pfm = pfm
+)
+
+
+# Run chromVAR
+
+gd_atac <- RunChromVAR(
+  object = gd_atac,
+  genome = BSgenome.Mmusculus.UCSC.mm10
+)
+
+DefaultAssay(gd_atac) <- 'chromvar'
+
+# figure 6a
+
+factor(Idents(gd_atac), levels= c(0,5,10,2,3,7,4,1,9,12,13,8,11,15,16,14,6))
+Idents(gd_atac) <- factor(Idents(gd_atac), levels= c(0,5,10,2,3,7,4,1,9,12,13, 8,11,15,16,14,6))
+
+dotplot.motif <-  c("MA0523.1","MA0768.1","MA1421.1","MA1522.1","MA0753.2","MA0769.2","MA1653.1","MA0442.2","MA1578.1","MA0688.1",
+                    "MA0689.1","MA0690.1","MA0800.1","MA0801.1","MA0802.1","MA0803.1","MA0805.1","MA0806.1","MA1567.1","MA0807.1",
+                    "MA0640.2","MA0473.3","MA0598.3","MA0761.2","MA0062.3","MA0750.2","MA0076.2","MA0764.2","MA0645.1","MA0765.2",
+                    "MA0051.1","MA0160.1","MA0517.1","MA0652.1","MA0653.1","MA0017.2","MA0772.1","MA1111.1","MA1418.1","MA1419.1",
+                    "MA0625.1","MA0624.1","MA1525.1","MA1420.1","MA1649.1","MA1547.1","MA1573.1","MA0493.1","MA1511.1","MA1515.1",
+                    "MA0071.1","MA1150.1","MA1151.1","MA0742.1","MA1516.1","MA1517.1","MA1532.1","MA0857.1","MA0038.2","MA0483.1",
+                    "MA0482.2","MA0842.2","MA0037.3","MA0117.2","MA0107.1","MA0101.1","MA0002.2","MA0511.2","MA0684.2","MA1112.2",
+                    "MA0476.1","MA0488.1","MA0496.3","MA1633.1","MA1622.1")
+
+DotPlot(gd_atac, features = dotplot.motif,cols = c("RdYlBu"), cluster.idents = F) + geom_point(aes(size=pct.exp), shape = 21, colour="black", stroke=0.5) + guides(size=guide_legend(override.aes=list(shape=21, colour="black", fill="white"))) + RotatedAxis() + scale_x_discrete( labels=ConvertMotifID(gd_atac,id = dotplot.motif, assay = "ATAC")) + theme(
   panel.background = element_blank(),
   panel.border = element_rect(fill = NA),
   text = element_text(size = 10),
@@ -15,46 +139,3 @@ DotPlot(multimodal_gd_gex, features = c("Sell","Ly6c2","Cd160","Gzmb","Klrg1","M
   panel.grid.major.y = element_line(color = "grey80") 
   
 )
-
-#figure 4b
-
-table_organ_by_celltypes <- multimodal_gd_gex@meta.data %>%
-  group_by(organ, celltypes) %>%
-  summarize(count = n()) %>%
-  spread(celltypes, count, fill = 0) %>%
-  ungroup() %>%
-  mutate(total_cell_count = rowSums(.[c(2:ncol(.))])) %>%
-  dplyr::select(c('organ', 'total_cell_count', everything())) %>%
-  arrange(factor(organ, levels = levels(multimodal_gd_gex@meta.data$organ)))
-
-library(dplyr)
-x <- c("LN","spleen","liver","lung","skin","LI","SI","pooled")
-
-table_organ_by_celltypes <- table_organ_by_celltypes %>% mutate(organ =  factor(organ, levels = x)) %>% arrange(organ) 
-temp_labels <- multimodal_gd_gex@meta.data %>% group_by(organ) %>% tally()
-temp_labels <-  temp_labels %>% mutate(organ =  factor(organ, levels = x)) %>% arrange(organ)
-
-table_organ_by_celltypes %>%
-  select(-c('total_cell_count')) %>%
-  reshape2::melt(id.vars = 'organ') %>%
-  ggplot(aes(organ, value)) +
-  geom_bar(aes(fill = variable), position = 'fill', stat = 'identity') +
-  geom_text(
-    data = temp_labels,
-    aes(x = organ, y = Inf, label = paste0('n = ', format(n, big.mark = ',', trim = TRUE)), vjust = -1),
-    color = 'black', size = 2.8
-  ) +
-  scale_fill_manual(name = 'celltypes', values = c('#FFC312','#C4E538','#12CBC4','#FDA7DF','grey','#ED4C67','#1289A7','#D980FA','#B53471')) +
-  scale_y_continuous(name = 'Percentage [%]', labels = scales::percent_format(), expand = c(0.01,0)) +
-  coord_cartesian(clip = 'off') +
-  theme_bw() +
-  theme(
-    legend.position = 'left',
-    plot.title = element_text(hjust = 0.5),
-    text = element_text(size = 16),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    axis.title.x = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
-    plot.margin = margin(t = 20, r = 0, b = 0, l = 0, unit = 'pt')
-  )
